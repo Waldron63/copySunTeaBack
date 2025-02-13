adff0e86b32c1b064884c9d3234384969e59f836
Santiago Gualdron <santiago.gualdron-r@mail.escuelaing.edu.co>
2025-02-12 22:23:53 -0500

    generacion del maven y el ambiente de trabajo

---
+++ src/main/java/edu/eci/cvds/ReservaLabECI/ReservaLabEci.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ReservaLabECI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaLabEci {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaLabEci.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/ReservaLabECI/ReservasLabEci.java
@@ -0,0 +1,12 @@
+package edu.eci.cvds.ReservaLabECI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaLabEciTests {
+	@Test
+	void contextLoads() {
+
+	}
+}
