a061fc403b3cc3e2c64fb8d17342d59a35cd4045
Alejandro Prieto <samuel.prieto-r@mail.escuelaing.edu.co>
2025-03-05 09:05:29 -0500

    Agregando getters y setters de clase User

--- src/main/java/edu/eci/cvds/labReserves/model/User.java
+++ src/main/java/edu/eci/cvds/labReserves/model/User.java
@@ -1,21 +1,74 @@
 package edu.eci.cvds.labReserves.model;
 
-import java.util.HashMap;
+import java.util.*;
 
 public abstract class User {
     private int id;
     private String name;
     private String mail;
     private String password;
-    private HashMap<Schedule,Reserve> reserves;
+    private HashSet<Reserve> reserves;
 
     public User(int id, String name, String mail, String password) {
         this.id = id;
         this.name = name;
         this.mail = mail;
         this.password = password;
-        this.reserves = new HashMap<>();
+        this.reserves = new HashSet<>();
     }
 
     
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public String getMail() {
+        return mail;
+    }
+
+
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+    public boolean agregarReserva(Reserve reserva){
+        reserves.add(reserva);
+        return true;
+    }
 }
