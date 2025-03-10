bc7e7c43283c148f6068035f1f8df2d96a6ac3a5
hakki17 <mariapaulasm2018@gmail.com>
2025-03-09 22:55:02 -0500

    clase ScheduleReference hecha

---
+++ src/main/java/edu/eci/cvds/labReserves/model/ScheduleReference.java
@@ -0,0 +1,159 @@
+package edu.eci.cvds.labReserves.model;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.time.LocalTime;
+
+public class ScheduleReference {
+    
+    private LocalTime openingTime;
+    private LocalTime closingTime;
+    private List<DayOfWeek> availableDays;
+    //private List<Schedule> reservedTimes;
+
+    public ScheduleReference() {
+        this.openingTime = null;
+        this.closingTime = null;
+        this.availableDays = new ArrayList<>();
+        //this.reservedTimes = new ArrayList<>();
+    }
+    
+    
+    public ScheduleReference(List<DayOfWeek> availableDays, LocalTime openingTime, LocalTime closingTime) {
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+        this.availableDays = availableDays;
+        //this.reservedTimes = new ArrayList<>();
+    }
+
+
+
+
+    public boolean isWithinSchedule(Schedule schedule) {
+        DayOfWeek scheduleDay = schedule.getDay().getDayOfWeek();
+        if (!availableDays.contains(scheduleDay)) {
+            return false;
+        }
+        return !schedule.getStartHour().isBefore(openingTime) && !schedule.getEndHour().isAfter(closingTime);
+    }
+
+    public boolean isAvailable(Schedule schedule) {
+        DayOfWeek day = schedule.getDay().getDayOfWeek();
+        LocalTime scheduleStartTime = schedule.getStartHour();
+        LocalTime scheduleEndTime = schedule.getEndHour();
+
+        boolean isDayAvailable = availableDays.contains(day);
+        boolean isTimeWithinRange = !scheduleStartTime.isBefore(this.openingTime) && !scheduleEndTime.isAfter(this.closingTime);
+
+        return isDayAvailable && isTimeWithinRange;
+    }
+
+
+    /*
+    
+    public boolean isWithinOperatingHours(Schedule schedule) {
+
+        boolean dayAvailable = availableDays.contains(schedule.getDay());
+    
+        if (!dayAvailable) {
+            return false;
+        }
+        
+        return !schedule.getStartHour().before(openingTime) && 
+               !schedule.getEndHour().after(closingTime);
+    }
+    
+
+    public boolean isTimeSlotAvailable(Schedule schedule) {
+        if (!isWithinOperatingHours(schedule)) {
+            return false;
+        }
+
+        if (!availableDays.contains(schedule.getDay())) {
+            return false;
+        }
+
+        // overlaps sirve para ver si dos horarios chocan
+        
+        for (Schedule reserved : reservedTimes) {
+            if (reserved.overlaps(schedule)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public boolean overlaps(Schedule other) {
+        if (!isSameDay(this.day, other.day)) {
+            return false;
+        }
+        
+        return !(this.endHour.before(other.startHour) || 
+                 this.startHour.after(other.endHour));
+    }
+    
+    
+    public boolean addReservedTimeSlot(Schedule schedule) {
+        if (isTimeSlotAvailable(schedule)) {
+            reservedTimes.add(schedule);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeReservedTimeSlot(Schedule schedule) {
+        return reservedTimes.remove(schedule);
+    }
+    
+    public void addAvailableDay(Date day) {
+        if (!availableDays.contains(day)) {
+            availableDays.add(day);
+        }
+    }
+    */
+
+
+
+    // Getters y setters
+    
+    public LocalTime getOpeningTime() {
+        return openingTime;
+    }
+    
+    public void setOpeningTime(LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+    
+    public LocalTime getClosingTime() {
+        return closingTime;
+    }
+    
+    public void setClosingTime(LocalTime closingTime) {
+        this.closingTime = closingTime;
+    }
+    
+    public List<DayOfWeek> getAvailableDays() {
+        return availableDays;
+    }
+    
+    public void setAvailableDays(List<DayOfWeek> availableDays) {
+        this.availableDays = availableDays;
+    }
+    
+    /*public List<Schedule> getreservedTimes() {
+        return reservedTimes;
+    }
+    
+    public void setreservedTimes(List<Schedule> reservedTimes) {
+        this.reservedTimes = reservedTimes;
+    }*/
+
+    public void addAvailableDay(DayOfWeek day) {
+        if (!availableDays.contains(day)) {
+            availableDays.add(day);
+        }
+    }
+}
