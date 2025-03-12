9522ba6a9fa931528bc2abf4c223825ffb94d920
hakki17 <mariapaulasm2018@gmail.com>
2025-03-11 21:27:46 -0500

    controlador labortory

--- src/main/java/edu/eci/cvds/labReserves/controller/LaboratoryController.java
+++ src/main/java/edu/eci/cvds/labReserves/controller/LaboratoryController.java
@@ -26,23 +26,14 @@ public class LaboratoryController {
         this.laboratoryService = laboratoryService;
     }
 
-    /**
-     * GET /api/laboratories : Obtener todos los laboratorios.
-     *
-     * @return ResponseEntity con estado 200 (OK) y la lista de laboratorios.
-     */
+
     @GetMapping
     public ResponseEntity<List<LaboratoryMongodb>> getAllLaboratories() {
         List<LaboratoryMongodb> laboratories = laboratoryService.getAllLaboratories();
         return new ResponseEntity<>(laboratories, HttpStatus.OK);
     }
 
-    /**
-     * GET /api/laboratories/{abbreviation} : Obtener un laboratorio por su abreviatura.
-     *
-     * @param abbreviation abreviatura del laboratorio a recuperar.
-     * @return ResponseEntity con estado 200 (OK) y el laboratorio, o 404 (No encontrado).
-     */
+
     @GetMapping("/{abbreviation}")
     public ResponseEntity<LaboratoryMongodb> getLaboratoryByAbbreviation(@PathVariable String abbreviation) {
         Optional<LaboratoryMongodb> laboratory = laboratoryService.getLaboratoryByAbbreviation(abbreviation);
@@ -50,12 +41,6 @@ public class LaboratoryController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    /**
-     * POST /api/laboratories : Crear un nuevo laboratorio.
-     *
-     * @param laboratory el laboratorio a crear.
-     * @return ResponseEntity con estado 201 (Creado) y el nuevo laboratorio.
-     */
     @PostMapping
     public ResponseEntity<LaboratoryMongodb> createLaboratory(@RequestBody LaboratoryMongodb laboratory) {
         LaboratoryMongodb createdLaboratory = laboratoryService.createLaboratory(laboratory);
@@ -63,12 +48,6 @@ public class LaboratoryController {
     }
 
 
-    /**
-     * DELETE /api/laboratories/{abbreviation} : Eliminar un laboratorio.
-     *
-     * @param abbreviation abreviatura del laboratorio a eliminar.
-     * @return ResponseEntity con estado 204 (Sin contenido).
-     */
     @DeleteMapping("/{abbreviation}")
     public ResponseEntity<Void> deleteLaboratory(@PathVariable String abbreviation) {
         // Suponiendo que hay un método de eliminación en el servicio.
