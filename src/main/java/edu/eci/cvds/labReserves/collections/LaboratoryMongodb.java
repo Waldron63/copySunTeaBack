e14f15e938a203c8a891b777703396a8869f5f78
hakki17 <mariapaulasm2018@gmail.com>
2025-03-16 21:26:00 -0500

    laboratormongodb perfecto

--- src/main/java/edu/eci/cvds/labReserves/collections/LaboratoryMongodb.java
+++ src/main/java/edu/eci/cvds/labReserves/collections/LaboratoryMongodb.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 import edu.eci.cvds.labReserves.model.ScheduleReference;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.annotation.Id;
 
 
 
@@ -16,11 +17,26 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @Document(collection = "laboratory")
 public class LaboratoryMongodb extends Laboratory {
 
+    @Id
+    private int id;
+
+    public LaboratoryMongodb(){
+        super();
+    }
+
     /**
      * Constructs a LaboratoryMongodb instance based on a Schedule object.
      */
     public LaboratoryMongodb(Laboratory laboratory) {
         super(laboratory.getName(), laboratory.getAbbreviation(), laboratory.getTotalCapacity(), laboratory.getLocation(), laboratory.getScheduleReferences());
+    }
+
+    /**
+     * Gets the id time of the laboratory.
+     * @return The id
+     */
+    public int getId(){
+        return id;
     }
 }
 
