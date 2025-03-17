c6a2f7ea1866589cdb1276b3917a8e664022aff6
hakki17 <mariapaulasm2018@gmail.com>
2025-03-16 21:26:54 -0500

    controlador de laboratory con los endpoints hechos y funcionando

--- src/main/java/edu/eci/cvds/labReserves/controller/LaboratoryController.java
+++ src/main/java/edu/eci/cvds/labReserves/controller/LaboratoryController.java
@@ -1,8 +1,10 @@
 package edu.eci.cvds.labReserves.controller;
 
 import edu.eci.cvds.labReserves.collections.LaboratoryMongodb;
+import edu.eci.cvds.labReserves.collections.ReserveMongodb;
 import edu.eci.cvds.labReserves.model.Schedule;
 import edu.eci.cvds.labReserves.model.Laboratory;
+import edu.eci.cvds.labReserves.model.ScheduleReference;
 import edu.eci.cvds.labReserves.services.LaboratoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,78 +26,90 @@ public class LaboratoryController {
     @Autowired
     private LaboratoryService laboratoryService;
 
-    // Crea un nuevo laboratorio
+    /**
+     * Crea un nuevo laboratorio.
+     * @param laboratory Objeto Laboratory a crear.
+     * @return ResponseEntity con el laboratorio creado y el estado HTTP.
+     */
     @PostMapping("/")
     public ResponseEntity<Laboratory> createLaboratory(@RequestBody Laboratory laboratory) {
         Laboratory createdLab = laboratoryService.createLaboratory(laboratory);
         return new ResponseEntity<>(createdLab, HttpStatus.CREATED);
     }
 
-    // Obtiene todos los laboratorios
-    @GetMapping("/")
-    public ResponseEntity<List<Laboratory>> getAllLaboratories() {
-        List<Laboratory> laboratories = laboratoryService.getAllLaboratories();
-        return new ResponseEntity<>(laboratories, HttpStatus.OK);
+    /**
+     * Obtiene la lista de todos los laboratorios.
+     * @return Lista de laboratorios almacenados.
+     */
+    @GetMapping("/laboratory")
+    public List<LaboratoryMongodb> getAllLaboratories() {
+        return laboratoryService.getAllLaboratories();
     }
 
-    // Obtiene un laboratorio por su abreviatura
-    @GetMapping("/{abbreviation}")
-    public ResponseEntity<Laboratory> getLaboratoryByAbbreviation(@PathVariable String abbreviation) {
-        Laboratory laboratory = laboratoryService.getLaboratoryByAbbreviation(abbreviation);
+    /**
+     * Retrieves a reserve by its ID.
+    @GetMapping("/{id}")
+    public ReserveMongodb getReserveById(@PathVariable int id) throws LabReserveException {
+        return reserveService.getReserveById(id);
+    }*/
 
-        if (laboratory != null) {
-            return new ResponseEntity<>(laboratory, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    /**
+     * Obtiene un laboratorio por su abreviatura.
+     * @param abbreviation Abreviatura del laboratorio.
+     * @return Objeto LaboratoryMongodb encontrado.
+     */
+    @GetMapping("/abbreviation/{abbreviation}")
+    public LaboratoryMongodb getLaboratoryByAbbreviation(@PathVariable String abbreviation) {
+        return laboratoryService.getLaboratoryByAbbreviation(abbreviation);    
     }
 
-    // Actualiza un laboratorio existente
-    @PutMapping("/{abbreviation}/update")
-    public ResponseEntity<Laboratory> updateLaboratory(@PathVariable String abbreviation, @RequestBody Laboratory laboratory) {
-        Laboratory updatedLab = laboratoryService.updateLaboratory(abbreviation, laboratory);
-
-        if (updatedLab != null) {
-            return new ResponseEntity<>(updatedLab, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    /**
+     * Actualiza un laboratorio existente basado en su abreviatura.
+     * @param abbreviation Abreviatura del laboratorio a actualizar.
+     */
+    @PutMapping("/update/{abbreviation}")
+    public void updateLaboratoryScheduleReference(@PathVariable String abbreviation) {
+         laboratoryService.updateLaboratoryScheduleReference(abbreviation);
     }
 
-    // Elimina un laboratorio
+    /**
+     * Elimina un laboratorio basado en su abreviatura.
+     * @param abbreviation Abreviatura del laboratorio a eliminar.
+     */
     @DeleteMapping("/{abbreviation}/byelaboratory")
-    public ResponseEntity<String> deleteLaboratory(@PathVariable String abbreviation) {
-        boolean deleted = laboratoryService.deleteLaboratory(abbreviation);
+    public void deleteLaboratory(@PathVariable String abbreviation) {
+        // boolean deleted = laboratoryService.deleteLaboratory(abbreviation);
 
-        if (deleted) {
+        /*if (deleted) {
             return new ResponseEntity<>("Laboratorio eliminado con éxito", HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Laboratorio no encontrado", HttpStatus.NOT_FOUND);
-        }
+        }*/
+        laboratoryService.deleteByAbbreviation(abbreviation);
     }
 
     // Agrega un horario disponible a un laboratorio
-    @PostMapping("/{abbreviation}/schedule")
-    public ResponseEntity<Laboratory> addAvailableDay(
+    /*@PutMapping("/{abbreviation}/schedule")
+    public void addAvailableDay(
             @PathVariable String abbreviation,
-            @RequestBody Map<String, String> requestBody) {
+                @RequestBody Map<String, String> requestBody) {
 
         // Convertir los valores del mapa a los tipos necesarios
-        DayOfWeek day = DayOfWeek.valueOf(requestBody.get("day"));
+        DayOfWeek day = DayOfWeek.valueOf(requestBody.get("day").toUpperCase());
         LocalTime openingTime = LocalTime.parse(requestBody.get("openingTime"));
         LocalTime closingTime = LocalTime.parse(requestBody.get("closingTime"));
 
-        Laboratory updatedLab = laboratoryService.addAvailableDay(abbreviation, day, openingTime, closingTime);
+        laboratoryService.addAvailableDay(abbreviation, day, openingTime, closingTime);
 
-        if (updatedLab != null) {
-            return new ResponseEntity<>(updatedLab, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
+    }*/
 
-    // Verifica disponibilidad de un laboratorio para un horario
-    @PostMapping("/{abbreviation}/availability")
+    /**
+     * Verifica la disponibilidad de un laboratorio para un horario específico.
+     * @param abbreviation Abreviatura del laboratorio.
+     * @param schedule Horario a verificar.
+     * @return ResponseEntity con la disponibilidad del laboratorio.
+     */
+    @GetMapping("/{abbreviation}/availability")
     public ResponseEntity<Map<String, Boolean>> checkAvailability(
             @PathVariable String abbreviation,
             @RequestBody Schedule schedule) {
